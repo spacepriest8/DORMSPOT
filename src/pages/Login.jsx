601fdd6e2d6e7acb27de0cc7ba00af02380d5a64
Okoronkwo Kingsley <96643230+spacepriest8@users.noreply.github.com>
2025-05-28 13:49:11 +0100

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const res = await fetch('http://localhost:5000/api/login', {
+     const res = await fetch('https://dormspot-backend.onrender.com/api/users/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
