0423a569497921bdb89bc23029a7a7893c33aab7
Okoronkwo Kingsley <96643230+spacepriest8@users.noreply.github.com>
2025-07-19 12:18:44 +0100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin:"https://dormspot-dkp2nmri0-priests-projects-f5137719.vercel.app",
+  origin:"http://localhost:5173",
   credentials: true,
   
 }));
