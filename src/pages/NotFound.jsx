52a9b10be91136a87f872401d625ac9b1a1462db
Okoronkwo Kingsley <96643230+spacepriest8@users.noreply.github.com>
2025-07-20 19:04:50 +0100

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,8 +5,7 @@ const NotFound = () => {
   return (
     <div className="not-found-container">
       <div className="not-found-content">
-        <h1>404 - Page Not Found</h1>
-        <p>The page you're looking for doesn't exist or has been moved.</p>
+        <p>You need to Login to View this page.</p>
         <div className="not-found-actions">
           <Link to="/" className="home-link">Return to Home</Link>
           <Link to="/login" className="browse-link">Login to find out more!</Link>
